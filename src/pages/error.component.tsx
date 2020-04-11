286dc3e642db7fffb04fafc6133674104a1aaf31
Kevin McGrady <kevinmcgrady47@gmail.com>
2020-04-11 09:28:15 +0100

    added error template

--- src/pages/error.component.tsx
+++ src/pages/error.component.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import urls from "../urls/urls";
+import { ErrorTemplate } from "../atomic/templates/404/404.component";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <p>Error page</p>
-    </div>
+    <ErrorTemplate
+      title="404"
+      errorMessage="Page not found!"
+      linkText="Go home"
+      linkURL={urls.homepage}
+    />
   );
 };
 
